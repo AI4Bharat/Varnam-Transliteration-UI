4467707f87cd340c38aa0492d81937d796924429
Gokul NC <gokulnc@ymail.com>
2020-05-02 22:27:19 +0530

    Final cleanup

--- README.md
+++ README.md
@@ -11,17 +11,3 @@ Contains Transliteration Editor (forked from [Varnam Project](https://github.com
 ### Serving on GCP App Engine
 1. `gcloud app deploy app.yaml`
 2. Optional: `gcloud app deploy dispatch.yaml` (To [link domain](https://stackoverflow.com/a/56619600))
-
-<hr/>
-
-## Original Instructions
-
-Files for www.varnamproject.com
-
-### Running
-* Install dependencies using `node install`
-* Run server using `node server.js`
-* You may need to compile the Varnam module yourself. Otherwise you may see errors like the following: `Error: dlopen(/Users/sdqali/src/play/varnamproject.com/node_modules/varnam.node, 1): no suitable image found.  Did find:`
-`	/Users/sdqali/src/play/varnamproject.com/node_modules/varnam.node: unknown file type, first eight bytes: 0x7F 0x45 0x4C 0x46 0x02 0x01 0x01 0x00`
-* To compile Varnam module, grab `https://github.com/navaneeth/libvarnam-nodejs` and build
-* Copy `<libvarnam-nodejs>/build/Release/varnam.node` to `./node_modules`
--- server.js
+++ server.js
@@ -2,39 +2,73 @@
 
 var express = require('express');
 var path    = require('path');
+var favicon = require('serve-favicon');
+var compress = require('compression');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 // var port    = process.env.VARNAM_WEB_PORT || 3000;
 var port = process.env.PORT || 3000;
 
 // Create "express" server.
-app  = express();
+app = express();
+
+// --------------------- ROUTES --------------------- //
 
 // Force redirect to HTTPS
-app.use(function(req, res, next){
-  if (req.host != 'localhost' && req.get('X-Forwarded-Proto') == 'http') {
-    res.redirect(`https://${req.host}${req.url}`);
+app.use(function(req, res, next) {
+  if (req.hostname != 'localhost' && req.get('X-Forwarded-Proto') == 'http') {
+    res.redirect(`https://${req.hostname}${req.url}`);
     return;
   }
-
-  app.router(req, res, next);
+  next();
 });
 
-app.configure(function(){
-  app.set('port', port);
-  app.set('root_directory', __dirname);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.set('view cache', true);
-  app.use(express.favicon());
-  app.use(express.compress());
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(app.router);
+app.set('port', port);
+app.set('root_directory', __dirname);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('view cache', true);
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use(compress());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
+app.use(cookieParser('ai4b-cookie'));
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'ai4b-session'
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+var functions = require('./lib/index.js');
+var helper = require('./lib/helpers.js');
+
+app.get('/editor', function(req, res) {
+  	res.render('editor');
 });
+
+app.get('/', function(req, res) {
+      res.render('index', { title: 'AI4Bharat Transliterator' });
+      // res.redirect("/editor");
+});
+
+app.get('/downloads', function (req, res) {
+	res.redirect("/docs/downloads");
+});
+app.get('/docs', functions.serveDocs);
+app.get('/docs/*', functions.serveDocs);
+
+// Handle 404 - Keep this as a last route
+app.use(function(req, res) {
+    res.status(404).sendFile(__dirname+'/views/404.html');
+  });
+
+// --------------------- END OF ROUTES --------------------- //
 
 //  terminator === the termination handler.
 function terminator(sig) {
@@ -54,8 +88,6 @@ process.on('exit', function() { terminator(); });
 ].forEach(function(element, index, array) {
     process.on(element, function() { terminator(element); });
 });
-
-var routes  = require('./routes')
 
 //  And start the app on that interface (and port).
 app.listen(port, function() {
